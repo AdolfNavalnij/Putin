2df6319833182ce5d72c97199c8ac5c68c2f12dd
AdolfNavalnij <60006461+AdolfNavalnij@users.noreply.github.com>
2020-05-30 11:29:33 +0300

    ss

--- putin.py
+++ putin.py
@@ -2,7 +2,7 @@ import discord
 import discord.ext.commands
 
 
-TOKEN = "NjY1MjU1NDM0OTAyNTY4OTg1"+".Xsl3yw.xfqQGWk7roKkYDDiK9-jS8jOU-0"
+TOKEN = "NjY1MjU1NDM0OTAyNTY4OTg1"+".XtIZQg.2fezchFXBNIepUTdkA3_oX7CVrY"
 bot = discord.ext.commands.Bot(command_prefix = "!")
 
 @bot.command()
